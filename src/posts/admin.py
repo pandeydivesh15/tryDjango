7bd08a3c6abab9caef8615b3f0a84bb50c772928
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2016-08-15 08:39:21 +0530

    Adds some customizations in posts display in admin page

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+	"""docstring for PostModelAdmin"""
+	class Meta:
+		model = Post
+	list_display=["title", "timestamp", "updated"]
+	list_display_links=["title"]
+	list_filter= ["timestamp","updated"]
+	search_fields =["title","content"]
+
+admin.site.register(Post, PostModelAdmin)
